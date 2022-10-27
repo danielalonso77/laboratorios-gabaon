65e054651d3b7060965c6874107f3b1477af286f
danielalonso77 <54615444+danielalonso77@users.noreply.github.com>
2022-10-27 17:20:03 -0500

    service worker

--- sw.js
+++ sw.js
@@ -1,63 +1,37 @@
-;
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_Pizzas',
-    urlsToCache = [
-        './',
-        'https://fonts.googleapis.com/css?family=Raleway:400,700',
-        'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
-        'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
-        'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
-        './style.css',
-        './script.js',
-        './img/1024.png',
-        './img/180.png'
-    ]
+ServiceWorkerContainer.register(scriptURL, options)
+  .then(function(ServiceWorkerRegistration) {  });
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(urlsToCache)
-                .then(() => self.skipWaiting())
+  if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }).catch(function(error) {
+      console.log('Service worker registration failed:', error);
+    });
+  } else {
+    console.log('Service workers are not supported.');
+  }
+
+// Path: sw.js
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+        return cache.addAll([
+            '/index.html',
+            '/style.css',
+            '/script.js'
+        ]);
         })
-        .catch(err => console.log('Falló registro de cache', err))
-    )
-})
+    );
+    });
 
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-self.addEventListener('activate', e => {
-    const cacheWhitelist = [CACHE_NAME]
-
-    e.waitUntil(
-        caches.keys()
-        .then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    //Eliminamos lo que ya no se necesita en cache
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName)
-                    }
-                })
-            )
+    self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
         })
-        // Le indica al SW activar el cache actual
-        .then(() => self.clients.claim())
-    )
-})
-
-//cuando el navegador recupera una url
-self.addEventListener('fetch', e => {
-    //Responder ya sea con el objeto en caché o continuar y buscar la url real
-    e.respondWith(
-        caches.match(e.request)
-        .then(res => {
-            if (res) {
-                //recuperar del cache
-                return res
-            }
-            //recuperar de la petición a la url
-            return fetch(e.request)
-        })
-    )
-})
+    );
+    });
+    
