14796adc29f184bf801c06147924c6cee529b849
Danielalonso77 <alonsoledesmal@gmail.com>
2022-12-11 20:13:41 -0600

    add_firebase

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
